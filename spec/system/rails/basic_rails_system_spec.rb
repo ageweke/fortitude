e568506f2acecd516e15a7d878278dbc2a57b91a
Andrew Geweke <andrew@geweke.org>
2014-01-26 19:14:18 -0800

    Yet more Rails integration tests.

--- spec/system/rails/basic_rails_system_spec.rb
+++ spec/system/rails/basic_rails_system_spec.rb
@@ -95,6 +95,21 @@ The data is:
     end
 
     it "should let you specify a widget with 'render :widget =>'"
+    it "should let you render a widget with 'render \"foo\"'"
+    it "should let you render a widget with 'render :file =>'"
+    it "should let you render a widget inline with 'render :inline =>'"
+  end
+
+  describe "rendering in a widget" do
+    it "should let you render :json in a widget"
+    it "should let you render :xml in a widget"
+    it "should let you render :js in a widget"
+  end
+
+  describe "render options" do
+    it "should let you set the content-type"
+    it "should let you set the location"
+    it "should let you set the status"
   end
 
   describe "rendering partial invocation" do
@@ -130,16 +145,34 @@ The data is:
     end
 
     it "should let you call an ERb partial from a widget with render :partial"
-    it "should let you render other things from a widget with render, like :json"
   end
 
   describe "layout integration" do
     it "should let you use a widget in an ERb layout, and render in the right order"
     it "should let you use a widget as a layout with an ERb view, and render in the right order"
     it "should let you use a widget as a layour with a widget view, and render in the right order"
+    it "should let you select the layout"
   end
 
   describe "helper integration" do
-    it "should let you "
+    it "should support the built-in Rails helpers by default"
+    it "should support both rendered and unrendered helpers properly"
+    it "should support custom-defined helpers"
+  end
+
+  describe "capture integration" do
+    it "should successfully capture a widget partial with capture { } in an ERb view"
+    it "should successfully capture an ERb partial with capture { } in a widget"
+    it "should be able to provide content in a widget with content_for"
+    it "should be able to provide content in a widget with provide"
+    it "should be able to retrieve stored content in a widget with content_for :name"
+    it "should be able to retrieve stored content in a widget with yield :name"
+  end
+
+  describe "translation integration" do
+    it "should let you translate strings with I18n.t"
+    it "should let you translate strings with just t"
+    it "should let you translate strings with Fortitude translation support"
+    it "should let you provide localized widgets"
   end
 end
