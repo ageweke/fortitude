efe1c2bfb9a0525e4cb7a65230706259cb788870
Andrew Geweke <andrew@geweke.org>
2014-02-01 08:58:30 -0800

    Put class in the right place.

--- spec/system/rails/class_loading_system_spec.rb
+++ spec/system/rails/class_loading_system_spec.rb
@@ -1,11 +1,9 @@
 describe "Rails class-loading support", :type => :rails do
   uses_rails_with_template :class_loading_system_spec
 
-  describe "error cases" do
-    it "should not allow you to put Foo::Bar in app/views/foo/bar.rb and make it work" do
-      expect_exception('the_class_should_not_load', 'NameError',
-        /uninitialized constant ClassLoadingSystemSpec::ClassShouldNotLoad/i)
-    end
+  it "should not load classes under app/views without the Views:: prefix" do
+    expect_exception('the_class_should_not_load', 'NameError',
+      /uninitialized constant ClassLoadingSystemSpec::ClassShouldNotLoad/i)
   end
 
   it "should allow me to define classes under Views:: outside of app/views, like in lib/views"
