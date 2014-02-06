4ea6ffced2e73ba17dbf6ddf7dfb776a69770c31
Andrew Geweke <andrew@geweke.org>
2014-02-05 17:35:22 -0800

    Fix render :widget => ... support.

--- lib/fortitude/railtie.rb
+++ lib/fortitude/railtie.rb
@@ -172,9 +172,8 @@ module Fortitude
       ::ActionController::Base.class_eval do
         def render_with_fortitude(*args, &block)
           if (options = args[0]).kind_of?(Hash) && (widget = args[0][:widget])
-            raise "kaboom"
-
-            rendering_context = ::Fortitude::RenderingContext.new
+            output = ""
+            rendering_context = ::Fortitude::RenderingContext.new(output, self)
             widget.to_html(rendering_context)
 
             options = options.dup
