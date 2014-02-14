af96f829d4e0741defec34e3384a8aa9ca2a3bed
Andrew Geweke <andrew@geweke.org>
2014-02-13 19:45:51 -0800

    Reorganize erector_compatibility_system_spec a bit.

--- spec/system/erector_compatibility_system_spec.rb
+++ spec/system/erector_compatibility_system_spec.rb
@@ -18,63 +18,67 @@ describe "Fortitude Erector compatibility", :type => :system do
     expect(ivars[:baz]).to eq("widget_baz")
   end
 
-  it "should not allow local access to needs via @foo, by default" do
-    wc = widget_class do
-      needs :foo
-      def content
-        text "foo: #{@foo.inspect}"
+  describe "@foo variable access" do
+    it "should not allow local access to needs via @foo, by default" do
+      wc = widget_class do
+        needs :foo
+        def content
+          text "foo: #{@foo.inspect}"
+        end
       end
+
+      expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: nil')
     end
 
-    expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: nil')
+    it "should not allow local access to needs via @foo, if turned off" do
+      wc = widget_class do
+        use_instance_variables_for_assigns false
+        needs :foo
+        def content
+          text "foo: #{@foo.inspect}"
+        end
+      end
+
+      expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: nil')
+    end
+
+    it "should allow local access to needs via @foo, if asked to" do
+      wc = widget_class do
+        use_instance_variables_for_assigns true
+        needs :foo
+        def content
+          text "foo: #{@foo}"
+        end
+      end
+
+      expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: the_foo')
+    end
   end
 
-  it "should not allow local access to needs via @foo, if turned off" do
-    wc = widget_class do
-      use_instance_variables_for_assigns false
-      needs :foo
-      def content
-        text "foo: #{@foo.inspect}"
+  describe "non-needed variables" do
+    it "should allow local access to non-needed passed variables via @foo, if asked to" do
+      wc = widget_class do
+        extra_assigns :use
+        use_instance_variables_for_assigns true
+        needs :foo
+        def content
+          text "foo: #{@foo}, "
+          text "bar: #{@bar}"
+        end
       end
+      expect(render(wc.new(:foo => "the_foo", :bar => "the_bar"))).to eq("foo: the_foo, bar: the_bar")
     end
 
-    expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: nil')
-  end
-
-  it "should allow local access to needs via @foo, if asked to" do
-    wc = widget_class do
-      use_instance_variables_for_assigns true
-      needs :foo
-      def content
-        text "foo: #{@foo}"
+    it "should allow local access to non-needed passed variables via method, if asked to" do
+      wc = widget_class do
+        extra_assigns :use
+        needs :foo
+        def content
+          text "foo: #{foo}, "
+          text "bar: #{bar}"
+        end
       end
+      expect(render(wc.new(:foo => "the_foo", :bar => "the_bar"))).to eq("foo: the_foo, bar: the_bar")
     end
-
-    expect(render(wc.new(:foo => 'the_foo'))).to eq('foo: the_foo')
-  end
-
-  it "should allow local access to non-needed passed variables via @foo, if asked to" do
-    wc = widget_class do
-      extra_assigns :use
-      use_instance_variables_for_assigns true
-      needs :foo
-      def content
-        text "foo: #{@foo}, "
-        text "bar: #{@bar}"
-      end
-    end
-    expect(render(wc.new(:foo => "the_foo", :bar => "the_bar"))).to eq("foo: the_foo, bar: the_bar")
-  end
-
-  it "should allow local access to non-needed passed variables via method, if asked to" do
-    wc = widget_class do
-      extra_assigns :use
-      needs :foo
-      def content
-        text "foo: #{foo}, "
-        text "bar: #{bar}"
-      end
-    end
-    expect(render(wc.new(:foo => "the_foo", :bar => "the_bar"))).to eq("foo: the_foo, bar: the_bar")
   end
 end
