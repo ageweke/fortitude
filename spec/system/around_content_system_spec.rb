3ff8b07a8b93c8521930c0fb5ea072d023206d4e
Andrew Geweke <andrew@geweke.org>
2014-02-15 17:21:42 -0800

    Lots of specs for around_content.

--- spec/system/around_content_system_spec.rb
+++ spec/system/around_content_system_spec.rb
@@ -1,6 +1,248 @@
-describe "Fortitude around_content operations" do
-  it "should operate with no around_content declared"
-  it "should run around_content methods in the order they are declared"
-  it "should run superclass around_content methods before subclass ones"
+describe "Fortitude around_content operations", :type => :system do
+  it "should operate with no around_content declared" do
+    wc = widget_class do
+      def content
+        text "foo"
+      end
+    end
+
+    expect(wc.send(:around_content_methods)).to eq([ ])
+    expect(render(wc.new)).to eq("foo")
+  end
+
+  it "should run an around_content method around the content method" do
+    wc = widget_class do
+      def content
+        text "content"
+      end
+
+      def around_content
+        text "around_before"
+        yield
+        text "around_after"
+      end
+
+      around_content :around_content
+    end
+
+    expect(render(wc.new)).to eq("around_beforecontentaround_after")
+  end
+
+  it "should still let #content call #yield properly" do
+    wc = widget_class do
+      def content
+        yield "aaa", "bbb"
+        text "content"
+      end
+
+      def around_content
+        text "around_before"
+        yield
+        text "around_after"
+      end
+
+      around_content :around_content
+    end
+
+    yielded = [ ]
+    rendering_context = rc(:yield_block => lambda { |*args| yielded << args; "" })
+    expect(render(wc.new, :rendering_context => rendering_context)).to eq("around_beforecontentaround_after")
+    expect(yielded).to eq([ [ "aaa", "bbb" ] ])
+  end
+
+  it "should not run the content method or other around_content methods if you don't call yield" do
+    wc = widget_class do
+      def content
+        text "content"
+      end
+
+      def around1
+        text "around1_before"
+        text "around1_after"
+      end
+
+      def around2
+        text "around2_before"
+        yield
+        text "around2_after"
+      end
+
+      around_content :around1, :around2
+    end
+
+    expect(render(wc.new)).to eq("around1_beforearound1_after")
+  end
+
+  it "should work even if you declare around_content after the method you're calling" do
+    wc = widget_class do
+      around_content :around1
+
+      def content
+        text "content"
+      end
+
+      def around1
+        text "around1_before"
+        yield
+        text "around1_after"
+      end
+    end
+
+    expect(render(wc.new)).to eq("around1_beforecontentaround1_after")
+  end
+
+  it "should propagate an exception out of the content method, using normal Ruby semantics" do
+    wc = widget_class do
+      def mark(token)
+        @marks ||= [ ]
+        @marks << token
+      end
+
+      def marks
+        @marks ||= [ ]
+      end
+
+      def content
+        mark(:content)
+        raise("kaboom")
+      end
+
+      def around1
+        mark(:around1_before)
+        yield
+        mark(:around1_after)
+      end
+
+      def around2
+        mark(:around2_before)
+        yield
+        mark(:around2_after)
+      end
+
+      around_content :around1, :around2
+    end
+
+    instance = wc.new
+    expect { render(instance) }.to raise_error("kaboom")
+    expect(instance.marks).to eq([ :around1_before, :around2_before, :content ])
+  end
+
+  it "should propagate an exception out of an around_content method, using normal Ruby semantics" do
+    wc = widget_class do
+      def mark(token)
+        @marks ||= [ ]
+        @marks << token
+      end
+
+      def marks
+        @marks ||= [ ]
+      end
+
+      def content
+        mark(:content)
+      end
+
+      def around1
+        mark(:around1_before)
+        yield
+        mark(:around1_after)
+      end
+
+      def around2
+        mark(:around2_before)
+        raise("kaboom")
+        yield
+        mark(:around2_after)
+      end
+
+      around_content :around1, :around2
+    end
+
+    instance = wc.new
+    expect { render(instance) }.to raise_error("kaboom")
+    expect(instance.marks).to eq([ :around1_before, :around2_before ])
+  end
+
+  it "should run around_content methods in the order they are declared" do
+    wc = widget_class do
+      def around1
+        text "around1_before"
+        yield
+        text "around1_after"
+      end
+
+      def around2
+        text "around2_before"
+        yield
+        text "around2_after"
+      end
+
+      def content
+        text "content"
+      end
+
+      around_content :around1
+      around_content :around2
+    end
+
+    expect(render(wc.new)).to eq("around1_beforearound2_beforecontentaround2_afteraround1_after")
+  end
+
+  it "should run superclass around_content methods before subclass ones" do
+    grandparent = widget_class do
+      def gp1
+        text "gp1_before"
+        yield
+        text "gp1_after"
+      end
+
+      def gp2
+        text "gp2_before"
+        yield
+        text "gp2_after"
+      end
+    end
+
+    parent = widget_class(:superclass => grandparent) do
+      def p1
+        text "p1_before"
+        yield
+        text "p1_after"
+      end
+
+      def p2
+        text "p2_before"
+        yield
+        text "p2_after"
+      end
+    end
+
+    child = widget_class(:superclass => parent) do
+      def c1
+        text "c1_before"
+        yield
+        text "c1_after"
+      end
+
+      def c2
+        text "c2_before"
+        yield
+        text "c2_after"
+      end
+
+      def content
+        text "content"
+      end
+    end
+
+    # we do it in this order to make sure the order in which they're declared as around_content methods
+    # doesn't make any difference
+    parent.around_content :p2, :p1
+    child.around_content :c1, :c2
+    grandparent.around_content :gp1, :gp2
+
+    expect(render(child.new)).to eq("gp1_beforegp2_beforep2_beforep1_beforec1_beforec2_beforecontentc2_afterc1_afterp1_afterp2_aftergp2_aftergp1_after")
+  end
+
   it "should allow two child classes to both inherit around_content methods of a parent, but add their own ones, as well"
 end
