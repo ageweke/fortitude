577c5a93aec72c93ae25cc6fd3e3d102e8e67621
Andrew Geweke <andrew@geweke.org>
2014-03-04 08:43:22 -0800

    Make sure the #fortitude_rendering_context is used even by partials.

--- spec/rails/rendering_context_system_spec.rb
+++ spec/rails/rendering_context_system_spec.rb
@@ -4,4 +4,8 @@ describe "Rails RenderingContext support", :type => :rails do
   it "should use the context returned by fortitude_rendering_context in the view" do
     expect_match("uses_specified_context_in_view", /context is: SimpleRc, value 12345/)
   end
+
+  it "should still use that context even for partials invoked by ERb" do
+    expect_match("uses_specified_context_in_partials", /before partial.*context is: SimpleRc, value 23456.*after partial/mi)
+  end
 end
--- spec/rails/templates/rendering_context_system_spec/app/controllers/rendering_context_system_spec_controller.rb
+++ spec/rails/templates/rendering_context_system_spec/app/controllers/rendering_context_system_spec_controller.rb
@@ -6,6 +6,11 @@ class RenderingContextSystemSpecController < ApplicationController
     @the_rendering_context_options = { :the_value => 12345 }
   end
 
+  def uses_specified_context_in_partials
+    @the_rendering_context_class = SimpleRc
+    @the_rendering_context_options = { :the_value => 23456 }
+  end
+
   def fortitude_rendering_context(options)
     @the_rendering_context_class.new((@the_rendering_context_options || { }).merge(options))
   end
