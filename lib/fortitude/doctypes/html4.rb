18340d0c76ab4e376fc6dd86e17318915f00f165
Andrew Geweke <andrew@geweke.org>
2014-03-20 19:48:36 -0700

    Lots more HTML4 tags.

--- lib/fortitude/doctypes/html4.rb
+++ lib/fortitude/doctypes/html4.rb
@@ -17,6 +17,9 @@ module Fortitude
       EVENTS_ATTRIBUTES = %w{onclick ondblclick onmousedown onmouseup onmouseover onmousemove onmouseout onkeypress onkeydown onkeyup}
       ATTRS_ATTRIBUTES = CORE_ATTRIBUTES + I18N_ATTRIBUTES + EVENTS_ATTRIBUTES
 
+      CELLHALIGN_ATTRIBUTES = %w{align char charoff}
+      CELLVALIGN_ATTRIBUTES = %w{valign}
+
       HEADING_CONTENT = %w{h1 h2 h3 h4 h5 h6}
       LIST_CONTENT = %w{ul ol}
       PREFORMATTED_CONTENT = %w{pre}
@@ -41,6 +44,7 @@ module Fortitude
       tag :title, :newline_before => true, :valid_attributes => I18N_ATTRIBUTES,
         :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.4.2'
       tag :meta, :newline_before => true, :valid_attributes => I18N_ATTRIBUTES + %w{http-equiv name content scheme},
+        :content_allowed => false,
         :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.4.4.2'
 
       # HTML4.01 spec, section 7.5
@@ -49,6 +53,123 @@ module Fortitude
         :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.1'
       tag :div, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => FLOW_CONTENT,
         :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.4'
+      tag :span, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.4'
+      tag :h1, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :h2, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :h3, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :h4, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :h5, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :h6, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.5'
+      tag :address, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/global.html#h-7.5.6'
+
+      # HTML4.01 spec, section 9.2
+      tag :em, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :strong, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :dfn, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :code, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :samp, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :kbd, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :var, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :cite, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :abbr, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :acronym, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.1'
+      tag :blockquote, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES + %w{cite},
+        :can_enclose => BLOCK_CONTENT + %w{script},
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.2'
+      tag :q, :valid_attributes => ATTRS_ATTRIBUTES + %w{cite}, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.2'
+      tag :sub, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.3'
+      tag :sup, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.2.3'
+
+      # HTML4.01 spec, section 9.3
+      tag :p, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.3.1'
+      tag :br, :newline_before => true, :valid_attributes => CORE_ATTRIBUTES, :content_allowed => false,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.3.2.1'
+      tag :pre, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES,
+        :can_enclose => INLINE_CONTENT - %w{img object big small sub sup},
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.3.4'
+      tag :ins, :valid_attributes => ATTRS_ATTRIBUTES + %w{cite datetime}, :can_enclose => FLOW_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.4'
+      tag :del, :valid_attributes => ATTRS_ATTRIBUTES + %w{cite datetime}, :can_enclose => FLOW_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/text.html#h-9.4'
+
+      # HTML4.01 spec, section 10.2
+      tag :ul, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => %w{li},
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.2'
+      tag :ol, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => %w{li},
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.2'
+      tag :li, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => FLOW_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.2'
+      tag :dl, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => %w{dt dd},
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.3'
+      tag :dt, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => INLINE_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.3'
+      tag :dd, :newline_before => true, :valid_attributes => ATTRS_ATTRIBUTES, :can_enclose => FLOW_CONTENT,
+        :spec => 'http://www.w3.org/TR/html401/struct/lists.html#h-10.3'
+
+      # HTML4.01 spec, section 11.2
+      tag :table, :newline_before => true, :can_enclose => %w{caption col colgroup thead tfoot tbody tr},
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{summary width border frame rules cellspacing cellpadding},
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.1'
+      tag :caption, :newline_before => true, :can_enclose => INLINE_CONTENT, :valid_attributes => ATTRS_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.2'
+      tag :thead, :newline_before => true, :can_enclose => %w{tr},
+        :valid_attributes => ATTRS_ATTRIBUTES + CELLVALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.3'
+      tag :tfoot, :newline_before => true, :can_enclose => %w{tr},
+        :valid_attributes => ATTRS_ATTRIBUTES + CELLVALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.3'
+      tag :tbody, :newline_before => true, :can_enclose => %w{tr},
+        :valid_attributes => ATTRS_ATTRIBUTES + CELLVALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.3'
+      tag :colgroup, :newline_before => true, :can_enclose => %w{col},
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{span width} + CELLHALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.4.1'
+      tag :col, :newline_before => true, :content_allowed => false,
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{span width} + CELLHALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.4.2'
+      tag :tr, :newline_before => true, :can_enclose => %w{th td},
+        :valid_attributes => ATTRS_ATTRIBUTES + CELLHALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.5'
+      tag :th, :newline_before => true, :can_enclose => FLOW_CONTENT,
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{abbr axis headers scope rowspan colspan} + CELLHALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.6'
+      tag :td, :newline_before => true, :can_enclose => FLOW_CONTENT,
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{abbr axis headers scope rowspan colspan} + CELLHALIGN_ATTRIBUTES + CELLVALIGN_ATTRIBUTES,
+        :spec => 'http://www.w3.org/TR/html401/struct/tables.html#h-11.2.6'
+
+      # HTML4.01 spec, section 12.3
+      tag :link, :newline_before => true, :content_allowed => false,
+        :valid_attributes => ATTRS_ATTRIBUTES + %w{charset href hreflang type rel rev media},
+        :spec => 'http://www.w3.org/TR/html401/struct/links.html#h-12.3'
+
+      # HTML4.01 spec, section 12.4
+      tag :base, :newline_before => true, :content_allowed => false, :valid_attributes => %w{href},
+        :spec => 'http://www.w3.org/TR/html401/struct/links.html#h-12.4'
+
+      # HTML4.01 spec, section 13.2
+
     end
   end
 end
