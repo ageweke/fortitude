1338ebef44a3b6650de9fc06e9ec23f7f97b6315
Andrew Geweke <andrew@geweke.org>
2014-04-17 21:09:08 -0700

    Add spec to make sure method precedence works correctly in Fortitude.

---
+++ spec/system/method_precedence_system_spec.rb
@@ -0,0 +1,48 @@
+describe "Fortitude method precedence", :type => :system do
+  it "should have widget methods > need methods > helper methods > tag methods" do
+    helpers_class = Class.new do
+      def foo
+        "helper_foo"
+      end
+
+      def bar
+        "helper_bar"
+      end
+
+      def baz
+        "helper_baz"
+      end
+
+      def quux
+        "helper_quux"
+      end
+    end
+
+    helpers_object = helpers_class.new
+
+    wc = widget_class do
+      tag :foo
+      tag :bar
+      tag :baz
+      tag :quux
+
+      helper :foo, :bar, :baz
+
+      needs :foo => 'need_foo', :bar => 'need_bar'
+
+      def foo
+        "method foo"
+      end
+
+      def content
+        text "foo: #{foo}, "
+        text "bar: #{bar}, "
+        text "baz: #{baz}, "
+        quux
+      end
+    end
+
+    expect(render(wc, :rendering_context => rc(
+      :helpers_object => helpers_object))).to eq("foo: method foo, bar: need_bar, baz: helper_baz, <quux/>")
+  end
+end
