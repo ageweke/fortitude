72291370907c5411e7228aab793ac08e51593bb9
Andrew Geweke <andrew@geweke.org>
2014-04-18 21:59:30 -0700

    Use String#original_concat, not String#<<, in our pure-Ruby extensions -- it's much faster; it doesn't do any #html_safe? checking.

--- lib/fortitude/fortitude_ruby_ext.rb
+++ lib/fortitude/fortitude_ruby_ext.rb
@@ -5,9 +5,9 @@ require 'erb'
     raise ArgumentError, "You can only append to a String" unless output.kind_of?(String)
 
     if html_safe?
-      output << self
+      output.original_concat(self)
     else
-      output << ERB::Util.html_escape(self)
+      output.original_concat(ERB::Util.html_escape(self))
     end
   end
 end
@@ -33,25 +33,25 @@ end
         else raise ArgumentError, "You can only use a String as a prefix"
         end
 
-        new_prefix << FORTITUDE_HYPHEN
+        new_prefix.original_concat(FORTITUDE_HYPHEN)
         value.fortitude_append_as_attributes(target, new_prefix)
       else
-        target << FORTITUDE_SPACE
+        target.original_concat(FORTITUDE_SPACE)
 
         case prefix
-        when String then target << prefix
+        when String then target.original_concat(prefix)
         when nil then nil
         else raise ArgumentError, "You can only use a String as a prefix"
         end
 
         fortitude_append_to(key, target)
-        target << FORTITUDE_EQUALS_QUOTE
+        target.original_concat(FORTITUDE_EQUALS_QUOTE)
         fortitude_append_to(value, target)
-        target << FORTITUDE_QUOTE
+        target.original_concat(FORTITUDE_QUOTE)
       end
     end
 
-    output << target
+    output.original_concat(target)
   end
 
   private
@@ -60,11 +60,11 @@ end
     when String then object.fortitude_append_escaped_string(output)
     when Symbol then object.to_s.fortitude_append_escaped_string(output)
     when Array then object.each_with_index do |o,i|
-      output << " " if i > 0
+      output.original_concat(" ") if i > 0
       fortitude_append_to(o, output)
     end
     when nil then nil
-    when Integer then output << object.to_s
+    when Integer then output.original_concat(object.to_s)
     else object.to_s.fortitude_append_escaped_string(output)
     end
   end
