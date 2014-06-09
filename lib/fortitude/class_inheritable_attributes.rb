61381be929e584ff49b172f7cc3323b449f29604
Andrew Geweke <andrew@geweke.org>
2014-06-08 18:58:52 -0700

    Break out class-inheritable attributes into their own concern.

---
+++ lib/fortitude/class_inheritable_attributes.rb
@@ -0,0 +1,41 @@
+require 'active_support'
+require 'active_support/concern'
+
+module Fortitude
+  module ClassInheritableAttributes
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+      def _fortitude_class_inheritable_attribute(attribute_name, default_value, allowable_values)
+        metaclass = (class << self; self; end)
+        metaclass.send(:define_method, attribute_name) do |*args|
+          raise ArgumentError, "Invalid arguments: #{args.inspect}" if args.length > 1
+          instance_variable_name = "@_fortitude_#{attribute_name}"
+          if args.length == 0
+            return instance_variable_get(instance_variable_name) if instance_variable_defined?(instance_variable_name)
+            return superclass.send(attribute_name) if superclass.respond_to?(attribute_name)
+            raise "Fortitude class-inheritable attribute error: there should always be a declared value for #{attribute_name} at the top of the inheritance hierarchy somewhere"
+          else
+            new_value = args[0]
+            allowed = if allowable_values.respond_to?(:call)
+              allowable_values.call(new_value)
+            else
+              allowable_values.include?(new_value)
+            end
+
+            if (! allowed)
+              error = "#{attribute_name} cannot be set to #{new_value.inspect}"
+              error << "; valid values are: #{allowable_values.inspect}" unless allowable_values.respond_to?(:call)
+              raise ArgumentError, error
+            end
+            instance_variable_set(instance_variable_name, new_value)
+            changed_method = "_fortitude_#{attribute_name}_changed!"
+            send(changed_method, new_value) if respond_to?(changed_method)
+            new_value
+          end
+        end
+        send(attribute_name, default_value)
+      end
+    end
+  end
+end
--- lib/fortitude/widget/base.rb
+++ lib/fortitude/widget/base.rb
@@ -1,3 +1,4 @@
+require 'fortitude/class_inheritable_attributes'
 require 'fortitude/tag'
 require 'fortitude/tags_module'
 require 'fortitude/errors'
@@ -27,39 +28,7 @@ module Fortitude
       end
 
       extend Fortitude::TagStore
-
-      class << self
-        def _fortitude_class_inheritable_attribute(attribute_name, default_value, allowable_values)
-          metaclass = (class << self; self; end)
-          metaclass.send(:define_method, attribute_name) do |*args|
-            raise ArgumentError, "Invalid arguments: #{args.inspect}" if args.length > 1
-            instance_variable_name = "@_fortitude_#{attribute_name}"
-            if args.length == 0
-              return instance_variable_get(instance_variable_name) if instance_variable_defined?(instance_variable_name)
-              return superclass.send(attribute_name) if superclass.respond_to?(attribute_name)
-              raise "Fortitude class-inheritable attribute error: there should always be a declared value for #{attribute_name} at the top of the inheritance hierarchy somewhere"
-            else
-              new_value = args[0]
-              allowed = if allowable_values.respond_to?(:call)
-                allowable_values.call(new_value)
-              else
-                allowable_values.include?(new_value)
-              end
-
-              if (! allowed)
-                error = "#{attribute_name} cannot be set to #{new_value.inspect}"
-                error << "; valid values are: #{allowable_values.inspect}" unless allowable_values.respond_to?(:call)
-                raise ArgumentError, error
-              end
-              instance_variable_set(instance_variable_name, new_value)
-              changed_method = "_fortitude_#{attribute_name}_changed!"
-              send(changed_method, new_value) if respond_to?(changed_method)
-              new_value
-            end
-          end
-          send(attribute_name, default_value)
-        end
-      end
+      include Fortitude::ClassInheritableAttributes
 
       _fortitude_class_inheritable_attribute :format_output, false, [ true, false ]
       _fortitude_class_inheritable_attribute :extra_assigns, :ignore, [ :error, :ignore, :use ]
