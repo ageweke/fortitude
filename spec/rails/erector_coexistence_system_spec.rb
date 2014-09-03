68c6e3b093c4993adfa19efc4de6c0cabc4ed8b6
Andrew Geweke <andrew@geweke.org>
2014-09-02 20:46:01 -0700

    Add Erector as a Gemfile dependency, so that Travis will pass.

--- spec/rails/erector_coexistence_system_spec.rb
+++ spec/rails/erector_coexistence_system_spec.rb
@@ -1,19 +1,33 @@
 describe "Erector coexistence support", :type => :rails do
   uses_rails_with_template :erector_coexistence_system_spec, :additional_gemfile_lines => "gem 'erector'"
 
-  it "should be able to render a Fortitude widget in app/views" do
-    expect_match("fortitude_widget_in_app_views", /this is Fortitude: foo = bar/)
+  begin
+    gem 'erector'
+  rescue Gem::LoadError => le
+    # ok
   end
 
-  it "should be able to render a Fortitude widget in app/v/views" do
-    expect_match("fortitude_widget_in_app_v_views", /this is Fortitude: foo = marph/)
+  begin
+    require 'erector'
+  rescue LoadError => le
+    # ok
   end
 
-  it "should be able to render an Erector widget in app/views" do
-    expect_match("erector_widget_in_app_views", /<p\s+class\s*=\s*"some_class"\s*>this is Erector: foo = baz<\/p>/)
-  end
+  if defined?(::Erector::Widget)
+    it "should be able to render a Fortitude widget in app/views" do
+      expect_match("fortitude_widget_in_app_views", /this is Fortitude: foo = bar/)
+    end
 
-  it "should be able to render an Erector widget in app/v/views" do
-    expect_match("erector_widget_in_app_v_views", /<p\s+class\s*=\s*"some_class"\s*>this is Erector: foo = quux<\/p>/)
+    it "should be able to render a Fortitude widget in app/v/views" do
+      expect_match("fortitude_widget_in_app_v_views", /this is Fortitude: foo = marph/)
+    end
+
+    it "should be able to render an Erector widget in app/views" do
+      expect_match("erector_widget_in_app_views", /<p\s+class\s*=\s*"some_class"\s*>this is Erector: foo = baz<\/p>/)
+    end
+
+    it "should be able to render an Erector widget in app/v/views" do
+      expect_match("erector_widget_in_app_v_views", /<p\s+class\s*=\s*"some_class"\s*>this is Erector: foo = quux<\/p>/)
+    end
   end
 end
