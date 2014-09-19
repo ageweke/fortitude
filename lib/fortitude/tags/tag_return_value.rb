97a12a083995d1cc8ccff7ac82e6a891eebdce9e
Andrew Geweke <andrew@geweke.org>
2014-09-18 18:47:26 -0700

    Make the tag return value inherit from Object, so that things like #is_a? work.

--- lib/fortitude/tags/tag_return_value.rb
+++ lib/fortitude/tags/tag_return_value.rb
@@ -2,7 +2,8 @@ require 'fortitude/errors'
 
 module Fortitude
   module Tags
-    TAG_RETURN_VALUE_SUPERCLASS = if defined?(::BasicObject) then ::BasicObject else ::Object end
+    # TAG_RETURN_VALUE_SUPERCLASS = if defined?(::BasicObject) then ::BasicObject else ::Object end
+    TAG_RETURN_VALUE_SUPERCLASS = ::Object
 
     class TagReturnValue < TAG_RETURN_VALUE_SUPERCLASS
       def method_missing(name, *args)
