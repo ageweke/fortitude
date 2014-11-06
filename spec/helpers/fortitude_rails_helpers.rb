075bfa212c34bcfe70caed90d170a44a5b9488f3
Andrew Geweke <andrew@geweke.org>
2014-11-05 16:46:23 -0800

    Move the last of the Fortitude-specific stuff back into this gem.

--- spec/helpers/fortitude_rails_helpers.rb
+++ spec/helpers/fortitude_rails_helpers.rb
@@ -4,6 +4,16 @@ module Spec
       def rails_server_project_root
         @rails_server_project_root ||= File.expand_path(File.join(File.dirname(__FILE__), '../..'))
       end
+
+      def rails_server_additional_gemfile_lines
+        [
+          "gem 'fortitude', :path => '#{rails_server_project_root}'"
+        ]
+      end
+
+      def rails_server_default_version
+        ENV['FORTITUDE_SPECS_RAILS_VERSION']
+      end
     end
   end
 end
