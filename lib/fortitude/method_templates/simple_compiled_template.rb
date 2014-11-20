ab369135a90e612bfe3e99e1cca45647f9acd906
Andrew Geweke <andrew@geweke.org>
2014-11-19 17:26:58 -0800

    Further performance improvements for needs-building.

--- lib/fortitude/method_templates/simple_compiled_template.rb
+++ lib/fortitude/method_templates/simple_compiled_template.rb
@@ -17,20 +17,37 @@ module Fortitude
           "def result",
           "  output = ''"
         ]
+        pending_fixed_strings = [ ]
 
         File.read(source_file).split(/\r\n|\r|\n/).each do |line|
           needs_end = false
           line = line.chomp
 
           if line =~ /^(.*)\#\s*\:if\s*(.*?)\s*$/i
+            if pending_fixed_strings.length > 0
+              method_text_lines << "    output << <<EOS"
+              method_text_lines += pending_fixed_strings
+              method_text_lines << "EOS"
+              pending_fixed_strings = [ ]
+            end
+
             line = $1
             condition = $2
             method_text_lines << "  if #{condition}"
-            needs_end = true
+            method_text_lines << "    output << <<EOS"
+            method_text_lines << line
+            method_text_lines << "EOS"
+            method_text_lines << "  end"
+          else
+            pending_fixed_strings << line
           end
+        end
 
-          method_text_lines << "    output << \"#{line}\\n\""
-          method_text_lines << "  end" if needs_end
+        if pending_fixed_strings.length > 0
+          method_text_lines << "    output << <<EOS"
+          method_text_lines += pending_fixed_strings
+          method_text_lines << "EOS"
+          pending_fixed_strings = [ ]
         end
 
         method_text_lines << "  output"
@@ -51,10 +68,19 @@ module Fortitude
           @hash = h.symbolize_keys
         end
 
+        def define_hash_method!(name)
+          symbol_name = name.to_sym
+          string_name = name.to_s
+
+          metaclass = (class << self; self; end)
+          metaclass.send(:define_method, name) do
+            @hash[symbol_name]
+          end
+        end
+
         def method_missing(name, *args)
-          raise "Method missing: #{name.inspect} with arguments: #{args.inspect}" if args.length > 0
-          return @hash[name] if @hash.key?(name)
-          raise "Method missing: #{name.inspect}; have no match: #{@hash.keys.inspect}"
+          define_hash_method!(name)
+          send(name, *args)
         end
       end
 
