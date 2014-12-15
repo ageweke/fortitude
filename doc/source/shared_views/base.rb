93a1ac050728df27187ef88118f6cce320af1ddb
Andrew Geweke <andrew@geweke.org>
2014-12-15 08:51:21 -0800

    First testing of middleman for building documentation.

---
+++ doc/source/shared_views/base.rb
@@ -0,0 +1,12 @@
+module SharedViews
+  class Base < Fortitude::Widget
+    doctype :html5
+
+    format_output                 true
+    start_and_end_comments        true
+    debug                         true
+    enforce_element_nesting_rules true
+    enforce_attribute_rules       true
+    enforce_id_uniqueness         true
+  end
+end
---
+++ doc/source/test2.html.rb
@@ -0,0 +1,7 @@
+require 'source/shared_views/base'
+
+class Test2 < ::SharedViews::Base
+  def content
+    h1 "hi hi, test2d!"
+  end
+end
