8cf333f45a2510b75830cca4ca984bb5ab89bbe3
Adam Becker <ad@mbecker.cc>
2015-12-03 11:22:04 -0500

    Cacheing

---
+++ lib/fortitude/widget/caching.rb
@@ -0,0 +1,37 @@
+module Fortitude
+  class Widget
+    module Caching
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+        # PUBLIC API
+        def cacheable(opts = {})
+          if extra_assigns == :use
+            extra_assigns :ignore
+          end
+
+          static_keys = Array(opts.fetch(:key, []))
+          options = opts.fetch(:options, {})
+
+          define_method(:cache_contents) do |&block|
+            cache calculate_cache_dependencies(assigns, static_keys), options do
+              block.call
+            end
+          end
+
+          around_content :cache_contents
+        end
+      end
+
+      private
+
+      def calculate_cache_dependencies(assigns, static_keys)
+        (
+          assigns.to_a.sort_by(&:first).flatten +
+          static_keys +
+          [widget_locale]
+        )
+      end
+    end
+  end
+end
