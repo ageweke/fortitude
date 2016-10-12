6915a7f3c1928660a8a7ef641f5bae1afc5c6e17
Andrew Geweke <andrew@geweke.org>
2016-10-11 18:40:11 -0700

    Add a failing test case for #45.

--- spec/rails/templates/view_paths_system_spec/app/controllers/view_paths_system_spec_controller.rb
+++ spec/rails/templates/view_paths_system_spec/app/controllers/view_paths_system_spec_controller.rb
@@ -4,4 +4,12 @@ class ViewPathsSystemSpecController < ApplicationController
 
   def autoloading_from_added_view_path
   end
+
+  def added_view_path_from_controller
+    append_view_path(File.join(File.expand_path(::Rails.root), 'view_path_two'))
+  end
+
+  def added_view_path_from_controller_with_impossible_to_guess_name
+    append_view_path(File.join(File.expand_path(::Rails.root), 'view_path_two'))
+  end
 end
